3f9cf61f6ca915b36e0928351258baf278a6f58a
ObedDM <pacocobot2@gmail.com>
2025-02-06 00:10:36 -0600

    Added price display

--- NFT Slider/main.js
+++ NFT Slider/main.js
@@ -24,8 +24,8 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-async function fetchData() {
-    const response = await fetch("https://api.opensea.io/api/v2/collection/mutant-ape-yacht-club/nfts",
+async function fetchNftData() {
+    const response = await fetch("https://api.opensea.io/api/v2/collection/mutant-ape-yacht-club/nfts?limit=10",
     {
         method: "GET",
         headers:{
@@ -35,25 +35,34 @@ async function fetchData() {
 
     const data = await response.json();
 
-    console.log(data);
-    
-    data.nfts.forEach((nft) => {
-        console.log(nft.display_image_url);
+    return data
+  }
 
+  async function displayData() {
+    data = await fetchNftData();
+
+    let pricing = "Not currently listed for purchase";
+
+    for (let nft of data.nfts) {
+
+      let nft_price_data = await getPrice(nft.identifier);
+
+        if (nft_price_data.available) {
+            pricing = `From ${nft_price_data.listing_price} ${nft_price_data.coin_type}`;
+        }
+ 
         const swiperSlider = document.createElement("div");
         
         swiperSlider.classList.add("swiper-slide");
         swiperSlider.innerHTML = `<div class="nft-box">
                                     <img src="${nft.display_image_url}" class="nft-img" />
                                     <h2 class="title">${nft.identifier}</h2>
-                                    <p class="price">From 8.48 ETH</p>
+                                    <p class="price">${pricing}</p>
                                     <a href="${nft.opensea_url}" class="buy-btn">Buy Now</a>
                                 </div>`;
         document.querySelector(".swiper-wrapper").appendChild(swiperSlider);
-    });
+    }
 }
-
-fetchData();
 
 async function getPrice(nft_id) {
 
@@ -72,30 +81,19 @@ async function getPrice(nft_id) {
 
     if (Object.keys(data).length === 0) {
         available = false;
+        console.log(nft_id, "unavailable");
         return {available};
     }
 
-    if (available == true) {
+    else {
         const decimals = 10 ** (data.price.current.decimals);
         const listing_price = data.price.current.value / decimals;
         const coin_type = data.price.current.currency;
+
+        console.log(nft_id, listing_price, coin_type);
 
         return {available, listing_price, coin_type};
     }
 }
 
-async function fetchNftPrice() {
-
-    const nft_id = "8778"
-    const nft_price_data = await getPrice(nft_id);
-
-    if (nft_price_data.available === true) {
-        console.log(`${nft_id} currently listed for: ${nft_price_data.listing_price} ${nft_price_data.coin_type}`);
-    }
-
-    else {
-        console.log("Not currently listed for purchase");
-    }
-}
-
-fetchNftPrice();
+displayData();
